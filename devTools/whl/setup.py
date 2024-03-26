a6e80aae32f37384c1059a134e4f3cbf8a315af4
Segarra <astrowelding@gmail.com>
2024-03-26 10:08:15 -0500

    fixed whl/setup.py to correctly consolidate whl output, as well as pull the parser/ folder

--- devTools/whl/setup.py
+++ devTools/whl/setup.py
@@ -2,6 +2,7 @@ from setuptools import setup, find_packages
 import os
 import datetime
 import shutil
+import glob
 
 # project base directory
 base_dir = os.path.join(os.path.dirname(__file__), '..', '..')
@@ -14,23 +15,26 @@ with open(requirements_path) as f:
     required = f.read().splitlines()
 
 # Move the build artifacts to a target dir
-def move_build_artifcats(target_dir, base_dir, package_name):
-    # Paths for the artifacts
-    dist_dir = os.path.join(base_dir, 'devTools', 'whl', 'dist')
-    build_dir = os.path.join(base_dir, 'devTools', 'whl', 'build')
-    egg_info_dir = os.path.join(base_dir, 'devTools', 'whl', f'gc_parser_{timeNow}.egg-info')
-
+def move_build_artifcats(target_dir, base_dir):
     # Create the target dir if it does not exist
     if not os.path.exists(target_dir):
         os.makedirs(target_dir)
+
+    # Paths for the artifacts
+    dist_dir = os.path.join(base_dir, 'devTools', 'whl', 'dist')
+    build_dir = os.path.join(base_dir, 'devTools', 'whl', 'build')
+
+    # Find .egg-info dir
+    egg_info_dirs = glob.glob(os.path.join(base_dir, 'gc_parser_*.egg-info')) # This generates in base dir for some reason #TODO: Fix for quality of life but not needed for functionality
+    for egg_info_dir in egg_info_dirs:
+        if os.path.exists(egg_info_dir):
+            shutil.move(egg_info_dir, target_dir)
 
     # Move each item to the target dir
     if os.path.exists(dist_dir):
         shutil.move(dist_dir, target_dir)
     if os.path.exists(build_dir):
         shutil.move(build_dir, target_dir)
-    if os.path.exists(egg_info_dir):
-        shutil.move(egg_info_dir, target_dir)
 
 # Setup package
 package_name = f'gc-parser-{timeNow}'
@@ -50,4 +54,4 @@ setup(
 )
 # After exe consolidate files
 output_dir = os.path.join(base_dir, 'devTools', 'whl', 'whl_creation_output')
-move_build_artifcats(output_dir, base_dir, package_name)
+move_build_artifcats(output_dir, base_dir)
