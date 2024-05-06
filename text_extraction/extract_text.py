53d0b7b686ab3e894d0abbf1926211c73f88e6b7
Dakota Havel <dakotahavel@gmail.com>
2024-05-06 17:01:34 -0400

    added metadata support and example scripts for py and ipynb

--- text_extraction/extract_text.py
+++ text_extraction/extract_text.py
@@ -3,11 +3,21 @@ from pathlib import Path
 import re
 import traceback
 import typing
+import json
 
 from text_extraction import pdf_reader
 from text_extraction.file_utils import coerce_file_to_pdf
 from text_extraction import pages, paragraphs
 
+def read_metadata(meta_data_path) -> dict:
+    try:
+        with open(Path(meta_data_path)) as f_in:
+            data = json.load(f_in)
+    except:
+        print("Failed to load metadata, attempted at path:", meta_data_path)
+        return {}
+
+    return data
 
 def assign_f_name_fields(f_name, doc_dict):
     filename = (
@@ -27,12 +37,13 @@ def assign_f_name_fields(f_name, doc_dict):
         "doc_num", str(f_name).split(",")[0].split(" ")[-1])
 
 def extract_text(
-    filepath,
-    meta_dict = None
+    filepath: Path
 ):
     print("running extract_text on", filepath)
     try:
-        meta_dict = meta_dict if meta_dict is not None else {}
+        metadata_path = Path(str(filepath) + '.metadata')
+
+        meta_dict = read_metadata(metadata_path)
         doc_dict = {}
         doc_dict['meta_data'] = meta_dict 
 
@@ -60,18 +71,24 @@ def generate_extracted_text_from_directory(
     source: str,
 ) -> typing.Iterator[dict] :
 
-    if not Path(source).is_dir:
-        raise("A directory of files to parse is required")
+    if not Path(source).is_dir() or not Path(source).exists():
+        raise Exception("A directory of files to parse is required")
 
     print("Extracting text from the files in", source)
 
     filepaths = Path(source).glob("**/*")
     to_process = []
+    metadata_files = []
     excluded = []
+
 
     for path_item in filepaths:
         if not path_item.is_file():
             excluded.append(path_item)
+            continue
+
+        if path_item.suffix == ".metadata":
+            metadata_files.append(path_item)
             continue
 
         if path_item.suffix.lower() in (".pdf", ".html", ".txt"):
@@ -89,9 +106,10 @@ def generate_extracted_text_from_directory(
         excluded.append(path_item)
 
     print(f"{len(to_process)} items to extract text from")
+    print(f"{len(metadata_files)} metadata files identified")
     print(f"{len(excluded)} items were excluded, probably not of filetypes: .pdf, .html, .txt")
     if excluded:
-        print(excluded)
+        print([f.stem+f.suffix for f in excluded])
 
     for filepath in to_process:
         extracted_text_dict = extract_text(filepath=filepath)
